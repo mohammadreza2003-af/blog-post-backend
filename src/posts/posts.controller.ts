c364bd2161bb785ccf1acda274fa6289f9fbda38
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-12 15:30:20 +0430

    feat: create get posts handler

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get('{/:id}/')
+  getPosts(@Param('userId') userId: number) {
+    return this.postsService.findAll(userId);
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly userService: UsersService) {}
+  findAll(id: number) {
+    const user = this.userService.findOneById(id);
+    console.log(id);
+    return [
+      {
+        user,
+        title: 'test',
+        content: 'rest',
+      },
+    ];
+  }
+}
