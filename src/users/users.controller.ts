871a31fd0340001c7ed3b04bfad7b107005dbc2a
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-12 09:49:29 +0430

    feat: make param and query user handler

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('{/:id}/')
+  getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(id, typeof id);
+    console.log(limit, page);
+    return 'success';
+  }
+}
