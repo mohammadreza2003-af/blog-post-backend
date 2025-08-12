4f8d3d29408dda6ef750ee4de33ba51fe18dbd76
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-12 12:00:32 +0430

    feat: create updateUser dto

---
+++ src/users/dtos/get-users-param.dto.ts
@@ -0,0 +1,8 @@
+import { IsNumber, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+export class GetUsersParamDto {
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  id?: number;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,16 +9,17 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user-dto';
+import { GetUsersParamDto } from './dtos/get-users-param.dto';
 
 @Controller('users')
 export class UsersController {
   @Get('{/:id}/')
   getUsers(
-    @Param('id', ParseIntPipe) id: number | undefined,
+    @Param() getUserParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log(id, typeof id);
+    console.log(getUserParamDto, typeof getUserParamDto);
     console.log(limit, page);
     return 'success';
   }
