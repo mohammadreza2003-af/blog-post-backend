0c17b53e4a08180f5eed0dba90d73298c20ad623
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-13 17:08:23 +0430

    feat: create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,26 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        port: 5432,
+        username: 'postgres',
+        password: 'postgres',
+        database: 'blog-post',
+        host: 'localhost',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
