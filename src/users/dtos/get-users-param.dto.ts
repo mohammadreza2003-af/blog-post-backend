c8a81f42ad94313aa5f12fb2aa8c9bbf67eae455
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-13 10:02:15 +0430

    refactor: fix id param getUser

--- src/users/dtos/get-users-param.dto.ts
+++ src/users/dtos/get-users-param.dto.ts
@@ -3,7 +3,6 @@ import { Type } from 'class-transformer';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 export class GetUsersParamDto {
   @ApiPropertyOptional({
-    name: 'Id',
     description: 'User id',
     example: 1234,
   })
