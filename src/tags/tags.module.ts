d2f190cdafe06ea5f23e9dc45272859da572c260
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-08-15 16:00:50 +0430

    feat: create meta options resource

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TagsController } from './tags.controller';
-import { TagsService } from './tags.service';
 
 @Module({
   controllers: [TagsController],
-  providers: [TagsService]
+  providers: [],
 })
 export class TagsModule {}
