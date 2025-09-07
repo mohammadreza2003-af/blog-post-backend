1fed16de9defe26861eeb52dcd0a66942854acfd
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-09-07 22:03:30 +0430

    feat: create delete controller tag

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
 import { CreateTagDto } from './dtos/create-tag.dto';
 import { TagsService } from './providers/tags.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('tags')
 export class TagsController {
@@ -8,5 +16,17 @@ export class TagsController {
   @Post()
   createTag(@Body() createTagDto: CreateTagDto) {
     return this.tagsService.createTag(createTagDto);
+  }
+
+  @ApiOperation({
+    summary: 'Delete tag',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The tag deleted successfuly',
+  })
+  @Delete('/:id')
+  deleteTag(@Param('id', ParseIntPipe) id: number) {
+    return this.tagsService.deleteTag(id);
   }
 }
